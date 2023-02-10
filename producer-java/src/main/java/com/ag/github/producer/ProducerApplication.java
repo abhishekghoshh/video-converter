729e4174da230c376f822bc93bb302c5f84942fe
Abhishek Ghosh <ghoshabhishek1640@gmail.com>
2023-02-11 00:27:19 +0530

    commit

--- producer-java/src/main/java/com/ag/github/producer/ProducerApplication.java
+++ producer-java/src/main/java/com/ag/github/producer/ProducerApplication.java
@@ -1,18 +1,15 @@
 package com.ag.github.producer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.ag.github.core.configuration.Reactor;
-import com.ag.github.core.configuration.ReactorDefination;
 
 @SpringBootApplication
 @EnableScheduling
 @Reactor
-public class ProducerApplication implements CommandLineRunner {
+public class ProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
